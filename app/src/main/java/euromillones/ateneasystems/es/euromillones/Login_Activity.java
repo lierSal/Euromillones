f31b3c3eefbfa03e0f4757220897bd82f4ff0613
Cubel89 <p.cubel@gmail.com>
2015-02-02 17:47:56 +0100

    Cambios en #1, #2, #3, #4 y #12

---
+++ app/src/main/java/euromillones/ateneasystems/es/euromillones/Login_Activity.java
@@ -0,0 +1,255 @@
+package euromillones.ateneasystems.es.euromillones;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class Login_Activity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        /**
+         * Para versiones de Android superiores a la 2.3.7 necesitamos agregar estas lineas
+         * asi funcionara cualquier conexion exterior
+         */
+        if (android.os.Build.VERSION.SDK_INT > 10) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+        /**
+         * Declaración de componentes
+         */
+        final EditText et_user = (EditText) findViewById(R.id.et_user);
+        final EditText et_pass = (EditText) findViewById(R.id.et_pass);
+        final TextView tv_respuesta = (TextView) findViewById(R.id.tv_respuesta);
+        final CheckBox cb_recordar = (CheckBox) findViewById(R.id.cb_recordar);
+        Button btn_login = (Button) findViewById(R.id.btn_login);
+        Button btn_registro = (Button) findViewById(R.id.btn_registro);
+
+        /**
+         * Declaracion de variables
+         */
+        String passCodConfig = new String();
+        String user = new String();
+        final Intent actividadPrincipal = new Intent(this, MainActivity.class);//Esto lo ponemos aqui porque dentro del boton no funciona
+
+        /**
+         * Primero cargamos la informacion del archivo de configuracion
+         */
+        final SharedPreferences config = getSharedPreferences("RecordarDatos", Context.MODE_PRIVATE);
+        //Ahora decimos que si el valor de checkbox es true que cargue los datos si no, nada
+        //Le ponemos como segundo parametro un false para que nos devuelva false en caso de no
+        //existir ese parametro.
+        if (config.getBoolean("checkRecordarLogin", false)) {
+            Log.e("Entra en:", "Si que hay datos");
+            passCodConfig = config.getString("passCod", "");
+            user = config.getString("user", "");
+            if (autologin(user, passCodConfig)) {
+                //Abrir siguiente activity
+                startActivity(actividadPrincipal);
+            } else {
+                if (eliminarConfig()) {
+                    tv_respuesta.setText(R.string.tv_User_Error);
+                }
+
+            }
+        } else {
+            Log.e("Entra en:", "No que hay datos");
+            Log.e("Configuracion", String.valueOf(config.getBoolean("checkRecordarLogin", false)));
+        }
+
+        /**
+         * Funcion de los botones
+         */
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Hacemos un if para saber si comprobarUsuario nos devuelve un true, en caso afirmativo
+                //autorizamos el acceso
+                if (comprobarUsuario(String.valueOf(et_user.getText()), String.valueOf(et_pass.getText()))) {
+                    //Contraseña correcta
+                    //Comprobamos si quiere recoedar los datos
+                    if (cb_recordar.isChecked()) {
+                        Log.e("Entra:", "Cheked");
+                        //Quiere recordar los datos
+                        /**
+                         * Guardar datos si esta marcado el checkbox
+                         */
+                        SharedPreferences.Editor editor = config.edit();
+                        ZDatosTemporales DU = (ZDatosTemporales) getApplicationContext();
+                        editor.putString("id", DU.getIdUser());
+                        editor.putString("nombre", DU.getNombreUser());
+                        editor.putString("user", DU.getMailUser());
+                        editor.putString("nivel", DU.getNivelUser());
+                        editor.putBoolean("checkRecordarLogin", true);
+                        editor.commit();
+                        //Abrimos el nuevo activity
+
+                    } else {
+                        Log.e("Entra:", "NO Cheked");
+                        //No quiere recordar los datos
+                        SharedPreferences.Editor editor = config.edit();
+                        editor.putString("id", "");
+                        editor.putString("nombre", "");
+                        editor.putString("user", "");
+                        editor.putString("nivel", "");
+                        editor.putBoolean("checkRecordarLogin", false);
+                        editor.commit();
+                    }
+                    //Abrir siguiente activity
+                    startActivity(actividadPrincipal);
+
+                } else {
+                    //Usuario incorrecto
+                    tv_respuesta.setText(R.string.tv_User_Error);
+                }
+
+
+            }
+        });//Fin boton de login
+
+
+    }
+
+    /**
+     * Funcion para hacer la consulta de login
+     */
+    public Boolean comprobarUsuario(String user, String pass) {
+        //Declaramos Variables
+        JSONObject respuestaJSON = new JSONObject(); //Donde ira la respuesta
+        ZBaseDatos conectBD = new ZBaseDatos(); //Creamos una variable conectBD con la clase "ZBaseDatos"
+        JSONObject cadena = new JSONObject(); //Creamos un objeto de tipo JSON
+        String respuesta = new String(); //Respuesta para saber si es OK o Error
+        Boolean devovlerRespuesta = new Boolean(false); //Esto es lo que devolvera si es true o false
+        try {
+            cadena.put("tarea", "Login");//Le asignamos los datos que necesitemos
+            cadena.put("datos", user);//Le asignamos los datos que necesitemos
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        cadena.toString(); //Para obtener la cadena de texto de tipo JSON
+        /**
+         * ENVIAMOS CONSULTA
+         */
+        // Enviamos la consulta y metemos lo recibido dentro de la variable respuesta
+        respuestaJSON = conectBD.consultaSQL(cadena);
+        //Log.e("DATOS RECIBIDOS:", respuestaJSON.toString());
+        try {
+            //Ahora extraemos del JSON la parte "Respuesta" para saber si es un OK o un Error
+            respuesta = respuestaJSON.getString("Respuesta");
+            if (respuesta.equals("OK")) {
+                //Si es un OK, llamaremos a las CLASES ZMD5 para hacer la comprobacion del Pass
+                ZMD5 md5 = new ZMD5();
+                //Tambien llamamos a la clase ZDatosTemporales para guardar los datos recibidos
+                ZDatosTemporales datosUsuario = (ZDatosTemporales) getApplicationContext();
+                if (md5.comprobarMD5(pass, respuestaJSON.getString("pass"))) {
+                    Log.e("PASS", "Correcto");
+                    //Al ser correcto el pass, metemos los datos en las variables de la clase ZDatosTemporales
+                    datosUsuario.setIdUser(Integer.parseInt(respuestaJSON.getString("id")));
+                    datosUsuario.setMailUser(respuestaJSON.getString("mail"));
+                    datosUsuario.setNivelUser(respuestaJSON.getString("nivel"));
+                    datosUsuario.setNombreUser(respuestaJSON.getString("nombre"));
+                    datosUsuario.setPassUser(respuestaJSON.getString("pass"));
+                    devovlerRespuesta = true;
+                } else {
+                    Log.e("PASS", "Incorrecto");
+                    devovlerRespuesta = false;
+                }
+            } else {
+                devovlerRespuesta = false;
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return devovlerRespuesta;
+    }
+
+    /**
+     * Funcion Autologin
+     * Esta funcion es mas simple ya que toma en cuenta los datos guardados en la configuracion
+     */
+    public Boolean autologin(String user, String pass) {
+        //Declaramos Variables
+        JSONObject respuestaJSON = new JSONObject(); //Donde ira la respuesta
+        ZBaseDatos conectBD = new ZBaseDatos(); //Creamos una variable conectBD con la clase "ZBaseDatos"
+        JSONObject cadena = new JSONObject(); //Creamos un objeto de tipo JSON
+        String respuesta = new String(); //Respuesta para saber si es OK o Error
+        Boolean devovlerRespuesta = new Boolean(false); //Esto es lo que devolvera si es true o false
+        try {
+            cadena.put("tarea", "Login");//Le asignamos los datos que necesitemos
+            cadena.put("datos", user);//Le asignamos los datos que necesitemos
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        cadena.toString(); //Para obtener la cadena de texto de tipo JSON
+        /**
+         * ENVIAMOS CONSULTA
+         */
+        // Enviamos la consulta y metemos lo recibido dentro de la variable respuesta
+        respuestaJSON = conectBD.consultaSQL(cadena);
+        //Log.e("DATOS RECIBIDOS:", respuestaJSON.toString());
+        try {
+            //Ahora extraemos del JSON la parte "Respuesta" para saber si es un OK o un Error
+            respuesta = respuestaJSON.getString("Respuesta");
+            if (respuesta.equals("OK")) {
+                //Si es un OK, llamaremos a las CLASES ZMD5 para hacer la comprobacion del Pass
+                ZMD5 md5 = new ZMD5();
+                //Tambien llamamos a la clase ZDatosTemporales para guardar los datos recibidos
+                ZDatosTemporales datosUsuario = (ZDatosTemporales) getApplicationContext();
+                if (pass.equals(respuestaJSON.getString("pass"))) {
+                    Log.e("PASS", "Correcto");
+                    //Al ser correcto el pass, metemos los datos en las variables de la clase ZDatosTemporales
+                    datosUsuario.setIdUser(Integer.parseInt(respuestaJSON.getString("id")));
+                    datosUsuario.setMailUser(respuestaJSON.getString("mail"));
+                    datosUsuario.setNivelUser(respuestaJSON.getString("nivel"));
+                    datosUsuario.setNombreUser(respuestaJSON.getString("nombre"));
+                    datosUsuario.setPassUser(respuestaJSON.getString("pass"));
+                    devovlerRespuesta = true;
+                } else {
+                    Log.e("PASS", "Incorrecto");
+                    devovlerRespuesta = false;
+                }
+            } else {
+                devovlerRespuesta = false;
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return devovlerRespuesta;
+    }
+
+    /**
+     * Funcion Borrar Configuracion
+     */
+    public boolean eliminarConfig() {
+        final SharedPreferences config = getSharedPreferences("RecordarDatos", Context.MODE_PRIVATE);
+        //No quiere recordar los datos
+        SharedPreferences.Editor editor = config.edit();
+        editor.putString("id", "");
+        editor.putString("nombre", "");
+        editor.putString("user", "");
+        editor.putString("nivel", "");
+        editor.putBoolean("checkRecordarLogin", false);
+        editor.commit();
+        return true;
+    }
+
+
+}
---
+++ app/src/main/java/euromillones/ateneasystems/es/euromillones/ZDatosTemporales.java
@@ -0,0 +1,63 @@
+package euromillones.ateneasystems.es.euromillones;
+
+import android.app.Application;
+
+/**
+ * Created by cubel on 26/01/15.
+ */
+public class ZDatosTemporales extends Application {
+    /**
+     * Declaracion de Variables Temporales
+     */
+    private int idUser = 0;//ID del usuario
+    private String nombreUser = new String();//Nombre del Usuario
+    private String passUser = new String();//Pass del Usuario (No se si completo o solo la primera parte)
+    private String nivelUser = new String();//Nivel del Usuario
+    private String mailUser = new String();//Mail del Usuario
+
+    /**
+     * Funciones para recuperar los datos
+     */
+    public String getIdUser() {
+        return String.valueOf(idUser);
+    }
+
+    public String getNombreUser() {
+        return nombreUser;
+    }
+
+    public String getPassUser() {
+        return passUser;
+    }
+
+    public String getNivelUser() {
+        return nivelUser;
+    }
+
+    public String getMailUser() {
+        return mailUser;
+    }
+
+    /**
+     * Funciones para escribir datos en esta clase
+     */
+    public void setIdUser(int dato) {
+        this.idUser = dato;
+    }
+
+    public void setNombreUser(String dato) {
+        this.nombreUser = dato;
+    }
+
+    public void setPassUser(String dato) {
+        this.passUser = dato;
+    }
+
+    public void setNivelUser(String dato) {
+        this.nivelUser = dato;
+    }
+
+    public void setMailUser(String dato) {
+        this.mailUser = dato;
+    }
+}
