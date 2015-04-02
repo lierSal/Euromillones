87c44f7fca3f3c7957662ecd521e74adcf943d1a
Cubel89 <p.cubel@gmail.com>
2015-04-02 13:47:11 +0200

    - Cambio de colores (Android 5.0)

---
+++ app/src/main/java/euromillones/ateneasystems/es/euromillones/RestorePass.java
@@ -0,0 +1,17 @@
+package euromillones.ateneasystems.es.euromillones;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class RestorePass extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restore_pass);
+    }
+
+
+    
+}
